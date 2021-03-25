24c478703e557c4f040c10a82a8a13692965e34e
Patryk <patryk.wal@onet.pl>
2021-03-25 16:39:15 +0100

    fix bugs and impovement of tests

--- algorithms/aco.py
+++ algorithms/aco.py
@@ -5,28 +5,57 @@ import copy
 
 class ACO:
     def __init__(self, iterations=1000, n_ants=25, graph=Graph(),
-                 capacity_limit=6000, distance_limit=1000, seed=None,
-                 alfa=2, beta=5, ro=0.2, th=80):    # seed=12023050
+                 cars_limit=6, capacity_limit=6000, distance_limit=1000, seed=None,
+                 alfa=2, beta=5, ro=0.2, th=80):  # seed=12023050
         self.iterations = iterations                # amount of iterations
         self.n_ants = n_ants                        # number of ants
         self.graph = copy.deepcopy(graph)           # graph information
+        self.cars_limit = cars_limit                # cars limit
         self.capacity_limit = capacity_limit        # capacity limit
         self.distance_limit = distance_limit        # distance limit
-        self.best_solution = None                   # best solution
+        self.best_solution = None                   # best solutionalfa
         self.alfa = alfa                            # alfa parameter
         self.beta = beta                            # beta parameter
         self.ro = ro                                # ro parameter
         self.th = th                                # th parameter
-        if seed != 0:
+        if seed is not None:
             np.random.seed(seed)                    # set seed
+
+    def reset(self, base_graph):
+        self.graph = copy.deepcopy(base_graph)      # graph information
+        self.best_solution = None                   # best solutionalfa
+
+    def hypothesis_road_lenght(self, solution):
+        s_all = 0
+        l_roads = list()
+        for i in solution:
+            s = 0
+            a = 1
+            for j in i:
+                b = j
+                s = s + self.graph.edges(min(a, b), max(a, b))
+                a = b
+            b = 1
+            s = s + self.graph.edges(min(a, b), max(a, b))
+            l_roads.append(s)
+            s_all = s_all + s
+        result = 0
+        avg = float(s_all) / len(l_roads)
+        for l_road in l_roads:
+            result = abs(l_road - avg)
+        return result
 
     def find_solution(self):
         solution = list()
         vertices = copy.deepcopy(self.graph.vertices)
-        while(len(vertices) != 0):
+        cars_used = 0
+        rate = 0
+        back_capacity = 0
+        while(len(vertices) != 0 and cars_used < self.cars_limit):
             path = list()
             vertex = np.random.choice(vertices)
             capacity = self.capacity_limit - self.graph.demand[vertex]
+            distance = self.graph.edges(1, vertex)
             path.append(vertex)
             vertices.remove(vertex)
             while(len(vertices) != 0):
@@ -39,14 +68,28 @@ class ACO:
                 else:
                     probabilities = probabilities/np.sum(probabilities)
                     vertex = np.random.choice(vertices, p=probabilities)
-                capacity = capacity - self.graph.demand[vertex]
-                if(capacity > 0):
+                c = capacity - self.graph.demand[vertex]
+
+                a = path[len(path)-1]
+
+                if((c > 0) and
+                        (distance + self.graph.edges(min(a, vertex), max(a, vertex)) + self.graph.edges(1, vertex) <= self.distance_limit)):
                     path.append(vertex)
                     vertices.remove(vertex)
+                    distance += self.graph.edges(min(a, vertex),
+                                                 max(a, vertex))
+                    capacity = capacity - self.graph.demand[vertex]
                 else:
                     break
+
+            back_capacity += capacity
             solution.append(path)
-        return solution, self.rate_solution(solution)
+            cars_used += 1
+            distance += self.graph.edges(1, path[len(path)-1])
+            rate += distance
+        if (cars_used > self.cars_limit or len(vertices) > 0):
+            return solution, -1, -1
+        return solution, rate, back_capacity
 
     def rate_solution(self, solution):
         s = 0
@@ -78,26 +121,45 @@ class ACO:
                 for (e, v) in self.graph.pheromones().items()})
 
     def run(self):
+        result = list()
         for i in range(self.iterations):
             solutions = []
             for _ in range(self.n_ants):
-                solution = self.find_solution()
+                s, rate, back_capacity = self.find_solution()
+                solution = (
+                    s, rate, self.hypothesis_road_lenght(s), back_capacity)
                 if solution[1] > 0:
                     solutions.append(solution)
                     self.check_solution(solution)
             self.update_pheromone(solutions)
             print(str(i+1)+":\t"+str(int(self.best_solution[1])) +
-                  "\t"+str(self.graph.optimal_value))
-            yield(str(i+1)+":\t"+str(int(self.best_solution[1])) +
-                  "\t"+str(self.graph.optimal_value))
-        print("best_solution", self.best_solution[0])
-        yield str(self.best_solution)
+                  "\t"+str(self.graph.optimal_value) +
+                  "\t"+str(self.best_solution[2]) +
+                  "\t"+str(self.best_solution[3]))
+            result.append((i+1, self.best_solution))
+            # result.append(str(i+1)+"\t"+str(self.best_solution[0]) +
+            #               "\t"+str(self.best_solution[1]) +
+            #               "\t"+str(self.best_solution[2]) +
+            #               "\t"+str(self.best_solution[3])+'\n')
+        print("best solution "+str(self.best_solution))
+        result.append(self.best_solution)
+        # result.append("Best Solution:"+"\t"+str(self.best_solution[0]) +
+        #               "\t"+str(self.best_solution[1]) +
+        #               "\t"+str(self.best_solution[2]) +
+        #               "\t"+str(self.best_solution[3])+'\n')
+        result.append(self.graph.optimal_value)
+        # result.append('Optimal solution '+str(self.graph.optimal_value)+'\n')
+        return result
+
+    def start(self):
+        # for result in self.run():
+        #     print(result)
+        self.run()
 
 
 if __name__ == "__main__":
     g = Graph()
     g.generateGraph("E-n22-k4.txt")
-    # aco = ACO_MinMax(t_max = 5, graph=g, cars_limit=5, capacity_limit=g.capacity_limit, distance_limit=1000, iterations=1000)
     aco = ACO(graph=g, cars_limit=5, capacity_limit=g.capacity_limit,
-              distance_limit=1000, iterations=1000)
-    solution = aco.run()
+              distance_limit=1000, iterations=100)
+    aco.start()
